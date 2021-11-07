8577b4ea486857a7056430eff41a1e182372ee59
Alexey Zhuchkov <alexey@yandex.ru>
2021-11-07 05:55:16 +0300

    Translation by MyMemoryTranslator & TTS

--- vv.py
+++ vv.py
@@ -1,12 +1,18 @@
 from vosk import Model, KaldiRecognizer
 import deep_translator
+import time
+import pyttsx3
 import os
 import json
 import pyaudio
 
 M                       =   deep_translator.MyMemoryTranslator(source='ru',target='en')
+tts                     =   pyttsx3.init()
+EN_VOICE                =   'HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Speech\\Voices\\Tokens\\TTS_MS_EN-US_ZIRA_11.0'
+tts.setProperty('voice', EN_VOICE)
+#voices                  =   tts.getProperty('voices')
 #model                   =   Model(r"c:/Repos/VV/models/vosk-model-ru-0.10") # полный путь к модели
-model                  =   Model(r"c:/Repos/VV/models/vosk-model-small-ru-0.22") # полный путь к модели
+model                   =   Model(r"c:/Repos/VV/models/vosk-model-small-ru-0.22") # полный путь к модели
 rec                     =   KaldiRecognizer(model, 16000)
 p                       =   pyaudio.PyAudio()
 stream                  =   p.open(
@@ -17,15 +23,27 @@ stream                  =   p.open(
     frames_per_buffer   =   8000
 )
 stream.start_stream()
-
 while True:
-    data                =   stream.read(4000)
-    if len(data) == 0:
-        break
-    if rec.AcceptWaveform(data):
-        res             =   json.loads(rec.Result())
-        translated      =   M.translate(res["text"])
-        print(translated)
+    try:
+        # if tts.isBusy():
+        #     time.sleep(0.1)
+        #     continue
+        data            =   stream.read(8000)
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            res         =   json.loads(rec.Result())
+            if res["text"] == "":
+                continue
+            translated  =   M.translate(res["text"])
+            print(translated)
+            tts.say(translated)
+            tts.runAndWait()
+            tts.stop()
+    except(Exception):
+        #stream.stop_stream()
+        #stream.start_stream()
+        pass
 
 print(rec.FinalResult())
 #     print(rec.Result() if rec.AcceptWaveform(data) else rec.PartialResult())
