bbba9c683f05ff24d0330ebb2a39fffc5ff89f71
Alexey Zhuchkov <alexey@yandex.ru>
2021-11-07 05:12:26 +0300

    Translation by MyMemoryTranslator

--- vv.py
+++ vv.py
@@ -1,29 +1,31 @@
 from vosk import Model, KaldiRecognizer
+import deep_translator
 import os
 import json
 import pyaudio
 
-model               =   Model(r"c:/Repos/VV/models/vosk-model-ru-0.10") # полный путь к модели
-#model               =   Model(r"c:/Repos/VV/models/vosk-model-small-ru-0.22") # полный путь к модели
-rec                 =   KaldiRecognizer(model, 8000)
-p                   =   pyaudio.PyAudio()
-stream              =   p.open(
+M                       =   deep_translator.MyMemoryTranslator(source='ru',target='en')
+#model                   =   Model(r"c:/Repos/VV/models/vosk-model-ru-0.10") # полный путь к модели
+model                  =   Model(r"c:/Repos/VV/models/vosk-model-small-ru-0.22") # полный путь к модели
+rec                     =   KaldiRecognizer(model, 16000)
+p                       =   pyaudio.PyAudio()
+stream                  =   p.open(
     format              =   pyaudio.paInt16,
     channels            =   1,
-    rate                =   8000,
+    rate                =   16000,
     input               =   True,
     frames_per_buffer   =   8000
 )
 stream.start_stream()
 
-Text                =   ""
-
 while True:
-    data = stream.read(4000)
+    data                =   stream.read(4000)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-        print(rec.Result())#  else rec.PartialResult())
+        res             =   json.loads(rec.Result())
+        translated      =   M.translate(res["text"])
+        print(translated)
 
 print(rec.FinalResult())
 #     print(rec.Result() if rec.AcceptWaveform(data) else rec.PartialResult())
